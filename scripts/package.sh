b9865f196b2994bb52a915d7567a34cb05fae189
ele-we <88611011+ele-we@users.noreply.github.com>
2022-02-02 19:56:37 +0800

    Fix version

--- scripts/package.sh
+++ scripts/package.sh
@@ -4,6 +4,10 @@ set -e
 
 if [ -n "$DIST_VERSION" ]; then
     version=$DIST_VERSION
+#Mod
+elif [ -n "$npm_package_version" ]; then
+    version=$npm_package_version
+#Mod-End
 else
     version=`git describe --dirty --tags || echo unknown`
 fi
@@ -14,6 +18,14 @@ VERSION=$version yarn build
 # include the sample config in the tarball. Arguably this should be done by
 # `yarn build`, but it's just too painful.
 cp config.sample.json webapp/
+
+#Mod
+if [[ ${version} =~ ^v[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+(-.+)?$ ]]; then
+    echo ${version:1} > webapp/version.html
+else
+    echo ${version} > webapp/version.html
+fi
+#Mod-End
 
 mkdir -p dist
 cp -r webapp element-$version
